1d2a61ec011f36f727d0bce62b5b45487b5539b7
lokty <vasiljev.ivan@gmail.com>
2018-03-06 13:33:44 +0800

    Slide2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './scss/css/App.css';
 import logo from './images/logo.png';
+import slide1Back from './images/slide1_back.png';
+import slide2BackLeft from './images/slide2_left.svg';
+import slide2BackRight from './images/slide2_right.svg';
 
 class App extends Component {
   render() {
@@ -24,9 +27,10 @@ class App extends Component {
 
       	<div className="app">
 
-					<div className="slide slide1">
-            <div className="slide-content-wrapper">
-              <div className="plate">
+					<div className="slide slide1 border-bottom-inset">
+						<div className="background slide1-back"><img src={ slide1Back } /></div>
+            <div className="slide-content-wrapper slide1-content-wrapper">
+              <div className="plate plate1 border-top">
                 <div className="presenting">
                   PRESENTING
                 </div>
@@ -40,11 +44,29 @@ class App extends Component {
                     RECOGNITION</p>
                   </div>
                 </div>
+                <div className="fline-is">
+                  <p><strong>Fline</strong> is a text detection and recognition system,
+									based on our research in Artificial Intelligence</p>
+                </div>
               </div>
             </div>
 					</div>
 
-          <div className="slide">
+          <div className="slide slide2">
+						<div className="background slide2-back">
+							<img src={ slide1Back } />
+						</div>
+            <div className="slide-content-wrapper slide2-content-wrapper">
+							<div className="plate plate2 border-bottom">
+								<div className="plate-title">
+									Based on our research in AI
+								</div>
+								<div className="plate-text">
+									In the core of the system (both detection and recognition) lies Artificial Intelligence: it make hypothesys, tries to interpret the result, learns on its mistakes -- all the things, that AI should do. <br />
+									Concidering the complexity of the task, using AI is inevitable: it grants more sophisticated tools to classify data, rather than just plain heuristics and
+								</div>
+							</div>
+						</div>
           </div>
 
           <div className="slide">
